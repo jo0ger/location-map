de6e188813cc710cee375190ea1656d9dce774b3
Jooger <zzy1198258955@163.com>
2018-01-16 23:34:57 +0800

    [update] update code

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+/**
+ * @desc PM2 deploy
+ * @author Jooger <iamjooger@gmail.com>
+ * @date 16 Jan 2018
+ */
+
+'use strict'
+
+const packageInfo = require('./package.json')
+
+module.exports = {
+  deploy: {
+    production: {
+      user: 'root',
+      host: 'jooger.me',
+      ref: 'origin/master',
+      repo: packageInfo.repository.url,
+      path: '/root/www/' + packageInfo.name,
+    }
+  }
+}
